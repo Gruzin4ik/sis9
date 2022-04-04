8ff23b3b288882716ccdf7a02e60e2b105307286
Gruzin4ik <102985337+Gruzin4ik@users.noreply.github.com>
2022-04-04 19:40:37 +0300

    Add files via upload

---
+++ Sis_Shelia9/Form1.Designer.cs
@@ -0,0 +1,228 @@
+﻿namespace Sis_Shelia9
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.button13 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(0, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(205, 53);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "9.1. Получение пути к запущенному приложению";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(434, 21);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(823, 602);
+            this.listBox1.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(0, 59);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(205, 53);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "9.2. Получение пути к папке, из которого запущено приложение ";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(0, 118);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(205, 53);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "9.3. Запуск другого приложения ";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(0, 177);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(205, 53);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "9.4. Запуск приложения с параметрами";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(0, 236);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(205, 53);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "9.5. Запуск приложения с запретом на закрытие своего приложения ";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(0, 295);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(205, 53);
+            this.button6.TabIndex = 6;
+            this.button6.Text = "9.6. Закрытие всех копий программы Блокнот";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(0, 354);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(205, 53);
+            this.button7.TabIndex = 7;
+            this.button7.Text = " 9.7. Открытие файла в ассоциированном приложении";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(0, 413);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(205, 53);
+            this.button8.TabIndex = 8;
+            this.button8.Text = " 9.8. Получение числа процессоров в системе ";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(0, 472);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(205, 53);
+            this.button9.TabIndex = 9;
+            this.button9.Text = "9.9. Второй способ получения процессоров в системе";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(0, 531);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(205, 53);
+            this.button10.TabIndex = 10;
+            this.button10.Text = "9.10. Пауза в программе ";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(223, 0);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(205, 53);
+            this.button11.TabIndex = 11;
+            this.button11.Text = " 9.11. Получение списка всех процессов, запущенных в системе ";
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.button11_Click_1);
+            // 
+            // button12
+            // 
+            this.button12.Location = new System.Drawing.Point(223, 59);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(205, 53);
+            this.button12.TabIndex = 12;
+            this.button12.Text = "9.12. Получение списка оконных процессов";
+            this.button12.UseVisualStyleBackColor = true;
+            this.button12.Click += new System.EventHandler(this.button12_Click);
+            // 
+            // button13
+            // 
+            this.button13.Location = new System.Drawing.Point(223, 118);
+            this.button13.Name = "button13";
+            this.button13.Size = new System.Drawing.Size(205, 53);
+            this.button13.TabIndex = 13;
+            this.button13.Text = " 9.13. Получение списка определенных процессов ";
+            this.button13.UseVisualStyleBackColor = true;
+            this.button13.Click += new System.EventHandler(this.button13_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1269, 645);
+            this.Controls.Add(this.button13);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Button button13;
+    }
+}
+
---
+++ Sis_Shelia9/Form1.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sis_Shelia9
+{
+    public partial class Form1 : Form
+    {
+        private Process[] procs;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string appPath = Application.ExecutablePath;
+            
+            listBox1.Items.Add(appPath);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            listBox1.Items.Add(System.AppDomain.CurrentDomain.BaseDirectory);
+
+            listBox1.Items.Add(Application.StartupPath);
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            
+            Process proc = new Process();
+           
+            proc.StartInfo.FileName = @"Notepad.exe";
+            proc.StartInfo.Arguments = "";
+            proc.Start();
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+           
+            Process.Start("notepad.exe", "test.txt");
+            
+            Process.Start("iexplore.exe", "netsources.narod.ru");
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+           
+            ProcessStartInfo start_info = new ProcessStartInfo
+            (@"C:\windows\system32\notepad.exe");
+            start_info.UseShellExecute = false;
+            start_info.CreateNoWindow = true;
+            
+            Process proc = new Process(); 
+            proc.StartInfo = start_info;
+            
+            proc.Start();
+            
+            proc.WaitForExit();
+            MessageBox.Show("Код завершения: " + proc.ExitCode, "Завершение " +
+            "Код", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            procs = Process.GetProcessesByName("Notepad");
+            int i = 0;
+            while (i != procs.Length)
+            {
+                procs[i].Kill();
+                i++;
+                MessageBox.Show("Всего : " + i.ToString());
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            
+            string sysFolder =
+            Environment.GetFolderPath(Environment.SpecialFolder.System);
+            
+            ProcessStartInfo pInfo = new ProcessStartInfo();
+            
+            pInfo.FileName = sysFolder + @"\eula.txt";
+            
+            pInfo.UseShellExecute = true;
+            Process p = Process.Start(@"C:\Users\gruzi\source\repos\Sis_Shelia9\eula.txt.");
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            int am = Process.GetCurrentProcess().ProcessorAffinity.ToInt32();
+            int processorCount = 0;
+            while (am != 0)
+            {
+                processorCount++;
+                am &= (am - 1);
+            }
+            MessageBox.Show(processorCount.ToString());
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(
+          String.Format(
+          "Число процессоров: {0}",
+          Environment.ProcessorCount.ToString()));
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            
+            System.Threading.Thread.Sleep(3000);
+        }
+
+        private void button11_Click_1(object sender, EventArgs e)
+        {
+            foreach (Process p in Process.GetProcesses())
+                listBox1.Items.Add(p.ToString());
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            foreach (Process p in
+            Process.GetProcesses(System.Environment.MachineName))
+            {
+                if (p.MainWindowHandle != IntPtr.Zero)
+                {
+                    
+                    listBox1.Items.Add(p.ToString());
+                }
+            }
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+
+            listBox1.Items.Clear();
+            foreach (Process p in Process.GetProcessesByName("steam"))
+                listBox1.Items.Add(p.ToString());
+
+        }
+        
+    }
+}
